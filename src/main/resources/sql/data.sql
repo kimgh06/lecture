663e0df74ab1e6940c29853ed0a27cd11742d8ab
jyeonjyan <jyeonjyan.dev@gmail.com>
2024-05-19 20:10:39 +0900

    Auto Increment primary key id 지만, data.sql 에서 id를 미리 세팅하여 application rerun시 mock data를 중복으로 생성하지 않도록 함.

--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -1,11 +1,11 @@
-INSERT INTO `board` (`title`, `content`)
-VALUES ('Spring Boot Tutorial', 'This is a tutorial about Spring Boot...'),
-       ('Java Basics', 'In this post, we will cover the basics of Java...'),
-       ('SQL for Beginners', 'SQL is a standard language for managing data...'),
-       ('Understanding OOP', 'Object-Oriented Programming (OOP) is a programming paradigm...'),
-       ('Guide to Git', 'Git is a distributed version control system...'),
-       ('JavaScript Fundamentals', 'JavaScript is a high-level, interpreted programming language...'),
-       ('Python for Data Science', 'Python is a popular language for data science...'),
-       ('HTML and CSS Tips', 'HTML and CSS are the fundamental technologies for building Web pages...'),
-       ('Docker Introduction', 'Docker is an open platform for developing, shipping, and running applications...'),
-       ('Kubernetes Overview', 'Kubernetes is a portable, extensible, open-source platform for managing...');
+INSERT IGNORE INTO `board` (`id`, `title`, `content`)
+VALUES (1, 'Spring Boot Tutorial', 'This is a tutorial about Spring Boot...'),
+       (2, 'Java Basics', 'In this post, we will cover the basics of Java...'),
+       (3, 'SQL for Beginners', 'SQL is a standard language for managing data...'),
+       (4, 'Understanding OOP', 'Object-Oriented Programming (OOP) is a programming paradigm...'),
+       (5, 'Guide to Git', 'Git is a distributed version control system...'),
+       (6, 'JavaScript Fundamentals', 'JavaScript is a high-level, interpreted programming language...'),
+       (7, 'Python for Data Science', 'Python is a popular language for data science...'),
+       (8, 'HTML and CSS Tips', 'HTML and CSS are the fundamental technologies for building Web pages...'),
+       (9, 'Docker Introduction', 'Docker is an open platform for developing, shipping, and running applications...'),
+       (10, 'Kubernetes Overview', 'Kubernetes is a portable, extensible, open-source platform for managing...');
