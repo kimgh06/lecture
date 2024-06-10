046345f213f4f2255d49171da295135e348f11db
jyeonjyan <jyeonjyan.dev@gmail.com>
2024-06-10 18:12:45 +0900

    Add cache 와 cache를 통한 lock에 대해서 알아보았다

---
+++ src/test/java/com/example/demo/class6/DefenceConcurrencyTest.java
@@ -0,0 +1,24 @@
+package com.example.demo.class6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@SpringBootTest
+@ActiveProfiles("dev")
+class DefenceConcurencyTest {
+
+    @Autowired
+    private DefenceConcurrency defenceConcurrency;
+
+    @Test
+    void testConcurrency() {
+        String requestId = "orderRequestByA";
+        defenceConcurrency.doSomething(requestId);
+
+        assertThat(defenceConcurrency.doSomething(requestId)).isEqualTo("fail");
+    }
+}
